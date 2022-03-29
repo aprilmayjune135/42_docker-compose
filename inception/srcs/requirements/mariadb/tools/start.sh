145a4fd9f96ff78b3661fd686010bfaff3577952
Aileen Chen <aileenchen@AC-MacBookPro.local>
2022-03-29 16:31:45 +0200

    volume mount - wordpress doen

--- inception/srcs/requirements/mariadb/tools/start.sh
+++ inception/srcs/requirements/mariadb/tools/start.sh
@@ -1,17 +1,18 @@
 #!bin/bash
 
+# set permisison
+chown -R mysql:mysql /var/lib/mysql
+chmod 755 /var/lib/mysql
+
 # turn on bash's job control
 set -m
 
-# Start the primary process and put it in the background
-/usr/sbin/mysqld --user=root &
+# start the primary process and put it in the background
+/usr/sbin/mysqld --user=mysql &
 
-# Start the second process
+# start the second process
 ./setup_mysql.sh
 
-
-# bring the primary process back into the foreground
-# and leave it there
-fg %1
-
+# bring the primary process back into the foreground and leave it there
 # reference see: https://docs.docker.com/config/containers/multi-service_container/
+fg %1
--- inception/srcs/requirements/wordpress/tools/start.sh
+++ inception/srcs/requirements/wordpress/tools/start.sh
@@ -1,9 +1,17 @@
 #!bin/bash
-wp core install --allow-root --url="https://xchen.42.fr/" --path="/var/www/html/" --title="Don't Worry Be Happy!" --admin_user=${WORDPRESS_DB_USER} --admin_email="xchen@student.codam.nl" --admin_password=${WORDPRESS_DB_PASSWORD_USER} && php-fpm7.3 -R
 
-# Note:
-# 1. [wp core install...]
-#		install wordpress website
-# 2. [php-fpm7.3 -R]
-# 		Reference: https://manpages.debian.org/testing/php7.3-fpm/php-fpm7.3.8
-#		-R Allow pool to run as root (disabled by default)
+# build directory for wordpress download/install
+mkdir -p ${WORDPRESS_DIR}
+chmod -R 755 ${WORDPRESS_DIR}
+chmod -R www-data:www-data ${WORDPRESS_DIR}
+
+# download wordpress and config for mysql data
+wp core download --allow-root --path=${WORDPRESS_DIR}
+cp wp-config.php ${WORDPRESS_DIR}/wp-config.php
+
+# install wordpress website
+wp core install --allow-root --path=${WORDPRESS_DIR} --url=https://${DOMAIN_NAME} --title="Don't Worry Be Happy!" --admin_user=${WORDPRESS_DB_USER} --admin_email="xchen@student.codam.nl" --admin_password=${WORDPRESS_DB_PASSWORD_USER}
+
+# -R Allow pool to run as root (disabled by default)
+# Reference: https://manpages.debian.org/testing/php7.3-fpm/php-fpm7.3.8
+php-fpm7.3 -R
