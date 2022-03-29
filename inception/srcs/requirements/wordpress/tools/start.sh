321dfd2d3e91631710221e2eddeafd49d4aa4486
Aileen Chen <aileenchen@AC-MacBookPro.local>
2022-03-29 12:00:31 +0200

    wordpress initial website install done

--- inception/srcs/requirements/wordpress/tools/start.sh
+++ inception/srcs/requirements/wordpress/tools/start.sh
@@ -1,5 +1,9 @@
 #!bin/bash
-php-fpm7.3 -R
+wp core install --allow-root --url="https://xchen.42.fr/" --path="/var/www/html/" --title="Don't Worry Be Happy!" --admin_user=${WORDPRESS_DB_USER} --admin_email="xchen@student.codam.nl" --admin_password=${WORDPRESS_DB_PASSWORD_USER} && php-fpm7.3 -R
 
-# Reference: https://manpages.debian.org/testing/php7.3-fpm/php-fpm7.3.8
-#	-R Allow pool to run as root (disabled by default)
+# Note:
+# 1. [wp core install...]
+#		install wordpress website
+# 2. [php-fpm7.3 -R]
+# 		Reference: https://manpages.debian.org/testing/php7.3-fpm/php-fpm7.3.8
+#		-R Allow pool to run as root (disabled by default)
